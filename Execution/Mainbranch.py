763aa9b361a25f632f026e48e805e4d9e4d63763
FredDec97 <95378750+FredDec97@users.noreply.github.com>
2022-12-26 12:37:54 +0100

    Small changfes on some comments

--- Execution/Mainbranch.py
+++ Execution/Mainbranch.py
@@ -10,21 +10,20 @@ testing the result with the test dataset.
 This script accesses functions and classes from the func.py script.
 '''
 test = FromCSV  # generating an object from the FromCSV class
-test.tablename = "testTable"  # changing the an attribute value
-test.csvname = "test.csv"  # changing the an attribute value
+test.tablename = "testTable"  # changing an attribute value
+test.csvname = "test.csv"  # changing an attribute value
 ideal = Idealclass  # generating an object from the FromCSV class
-ideal.tablename ="idealTable"  # changing the an attribute value
-ideal.csvname = "ideal.csv"  # changing the an attribute value
+ideal.tablename = "idealTable"  # changing an attribute value
+ideal.csvname = "ideal.csv"  # changing an attribute value
 train = Trainclass  # generating an object from the FromCSV class
-train.tablename ="trainTable"  # changing the an attribute value
-train.csvname = "train.csv"  # changing the an attribute value
+train.tablename = "trainTable"  # changing an attribute value
+train.csvname = "train.csv"  # changing an attribute value
 sqliteConnection = sqlite3.connect('ForCalc.db')  # conencting to sqllite
 cursor = sqliteConnection.cursor()  # creating a cursor object using the cursor() method
-#testXX = "ForCalc.db"
 readCSVloadData(ideal.csvname, ideal.tablename, sqliteConnection, cursor, 51, 2)  # read and load data from CSV to db using a function, plots dataset
 readCSVloadData(train.csvname, train.tablename, sqliteConnection, cursor, 5, 2)  # read and load data from CSV to db using a function, plots dataset 
 readCSVloadData(test.csvname, test.tablename, sqliteConnection, cursor, 2, 1)  # read and load data from CSV to db using a function, plots dataset
-minvalue_series = identifyideal(sqliteConnection, train.tablename, ideal.tablename) # identifies the matching ideal functions and plots the results
+minvalue_series = identifyideal(train.tablename, ideal.tablename) # identifies the matching ideal functions and plots the results
 
 for n in range(0, 4):  # for loop the range of 0:4 stands for the 4 ideal functions
     for i in range(0, 100):  # for loop the range of 0:100 stands for the 100 test datasets 
@@ -36,8 +35,8 @@ for n in range(0, 4):  # for loop the range of 0:4 stands for the 4 ideal functi
         '''
         TestX, TestY, IdealY, DeltaY = mergeTestAndIdeal(int(i), minvalue_series[n], table_df)
         if float(DeltaY) < math.sqrt(2):  # Test if testing criteria is fullfilled
-            SaveRow(TestX, TestY, DeltaY, minvalue_series[n])  # call function, saves row if criteria is fullfilled
+            #SaveRow(TestX, TestY, DeltaY, minvalue_series[n])  # call function, saves row if criteria is fullfilled
+            print("x")
 print("Script finished successfully, please find the solution table with the name tresults in the Database")  # prints confirmation once script ran successfully
-
 
 # %%
